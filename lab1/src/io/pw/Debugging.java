3eaa5bcaf1477d0e7a80e8ce9dfec1bf60227596
dzon2000 <dzon2000@protonmail.com>
2021-10-09 22:53:30 +0200

    Lab 1 Debugging exercise

---
+++ lab1/src/io/pw/Debugging.java
@@ -0,0 +1,19 @@
+package io.pw;
+
+public class Debugging {
+
+    public static void main(String[] args) {
+        new Debugging().debugMe(Integer.parseInt("3E", 25));
+    }
+
+    private int debugMe(int x) {
+        int here = Integer.parseInt("npbl7p", 34);
+        if (x < 10) {
+            return 88;
+        } else if (x > 88) {
+            return 11;
+        }
+        return 12;
+    }
+
+}

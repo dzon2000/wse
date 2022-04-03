9de0ec8aeff4bdacd47c2db73c187c295be231bb
dzon2000 <dzon2000@protonmail.com>
2022-04-03 12:22:17 +0200

    Adding View and Model

---
+++ mvc/struts/src/main/java/io/pw/model/Greet.java
@@ -0,0 +1,17 @@
+package io.pw.model;
+
+/**
+ * Created by pwykowski
+ */
+public class Greet {
+
+	private String message;
+
+	public Greet(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}

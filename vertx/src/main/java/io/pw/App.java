54bb48f87ca40b91fb2ccfa1ff529e722d45846b
dzon2000 <33714273+dzon2000@users.noreply.github.com>
2022-05-21 21:11:16 +0200

    Simple Web Server

--- vertx/src/main/java/io/pw/App.java
+++ vertx/src/main/java/io/pw/App.java
@@ -1,14 +1,25 @@
 package io.pw;
 
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 
 /**
  * Created by pwykowski
  */
-public class App {
+public class SimpleWebServer extends AbstractVerticle {
 
 	public static void main(String[] args) {
 		Vertx vertx = Vertx.vertx();
+		vertx.deployVerticle(new SimpleWebServer());
+	}
+
+	@Override
+	public void start() throws Exception {
+		vertx.createHttpServer().requestHandler(req -> {
+			req.response()
+					.putHeader("content-type", "text/plain")
+					.end("Hello from Vert.x!");
+		}).listen(8080);
 	}
 
 }

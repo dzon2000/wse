bcdd55b848c1da3cc9106e6be095716c1f5e7284
dzon2000 <dzon2000@protonmail.com>
2022-05-21 20:02:55 +0200

    Vertx initial commit

---
+++ vertx/src/main/java/io/pw/App.java
@@ -0,0 +1,14 @@
+package io.pw;
+
+import io.vertx.core.Vertx;
+
+/**
+ * Created by pwykowski
+ */
+public class App {
+
+	public static void main(String[] args) {
+		Vertx vertx = Vertx.vertx();
+	}
+
+}

c306c9ddda41ecc5b65d3057bf0450d2257ca34a
dzon2000 <33714273+dzon2000@users.noreply.github.com>
2022-05-21 21:58:39 +0200

    Adding message when server starts

--- vertx/src/main/java/io/pw/event/Server.java
+++ vertx/src/main/java/io/pw/event/Server.java
@@ -2,16 +2,21 @@ package io.pw.event;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.MultiMap;
+import io.vertx.core.Promise;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
 
 /**
  * Created by pwykowski
  */
 public class Server extends AbstractVerticle {
 
+	private static final int PORT = 8080;
+	private HttpServer server;
+
 	@Override
-	public void start() throws Exception {
-		vertx.createHttpServer().requestHandler(req -> {
+	public void start(Promise<Void> startPromise) throws Exception {
+		this.server = vertx.createHttpServer().requestHandler(req -> {
 			//  Handle form by reading data and sending it to file verticle
 			if (req.path().equals("/persist") && req.method().equals(HttpMethod.POST)) {
 				req.setExpectMultipart(true);
@@ -38,7 +43,15 @@ public class Server extends AbstractVerticle {
 										"""
 						);
 			}
-		}).listen(8080);
+		});
+		this.server.listen(PORT, res -> {
+			if (res.succeeded()) {
+				System.out.println("Running HTTP server on port " + PORT);
+				startPromise.complete();
+			} else {
+				startPromise.fail(res.cause());
+			}
+		});
 	}
 
 }

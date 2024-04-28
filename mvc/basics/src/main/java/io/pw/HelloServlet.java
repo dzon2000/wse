500d78086809bad22ef02fea29014d053b7e82e6
dzon2000 <dzon2000@protonmail.com>
2024-04-28 09:00:31 +0200

    Adding doPost method

--- mvc/basics/src/main/java/io/pw/HelloServlet.java
+++ mvc/basics/src/main/java/io/pw/HelloServlet.java
@@ -7,6 +7,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by pwykowski
@@ -14,9 +18,23 @@ import java.io.IOException;
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
+	private static Map<String, String> USERS = new HashMap<>();
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		final String name = req.getParameter("name");
-		resp.getOutputStream().print("Hello, " + name);
+		final String brand = USERS.getOrDefault(name, "None");
+		resp.getOutputStream().print(String.format("""
+				<h1>Hello %s! Your favourite car brand is %s</h1>
+				""", name, brand));
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		final String name = req.getParameter("name");
+		final String brand = req.getParameter("brand");
+		System.out.printf("Name %s, brand %s", name, brand);
+		USERS.put(name, brand);
+		resp.getOutputStream().print("OK");
 	}
 }
